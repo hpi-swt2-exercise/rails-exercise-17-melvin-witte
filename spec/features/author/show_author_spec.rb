55347fba559559564a4954721ad13994a828aec5
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 14:42:19 +0100

    Add failing test for #9: \Author page should display author details'

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -3,11 +3,18 @@ require 'rails_helper'
 describe "Show author page", type: :feature do
  
 	it "should display the author's details" do
-		# FactoryGirl.find_definitions
 		author = create(:author)
 		visit author_path(author)
 		expect(page).to have_content(author.first_name)
 		expect(page).to have_content(author.last_name)
 		expect(page).to have_content(author.homepage)
-	end	
+	end
+
+	it "should display the author's details in a very specific way" do
+		author = create(:author)
+		visit author_path(author)
+		expect(page).to have_content("First name: " + author.first_name)
+		expect(page).to have_content("Last name: " + author.last_name)
+		expect(page).to have_content("Homepage: " + author.homepage)
+	end
 end
