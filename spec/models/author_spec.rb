41b05a94a40bb80fc4ebee6ea255795cc1b86382
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 13:42:01 +0100

    Add failing test for #6: Author #name should return the full name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,9 @@ describe "Author model", type: :model do
 		expect(author.last_name).to eq('Turing')
 		expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
 	end
+
+	it "should have a method to combine first name and last name" do
+		author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+		expect(author.name).to eq('Alan Turing')
+	end
 end
