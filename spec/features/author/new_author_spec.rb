47bc7fc8e868740e29d71be5ab49e9c2091ccbe2
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 12:12:56 +0100

    Delete unused tests, add failing test for #2: 'New author page should have text input for first name, last name, and homepage'

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
  
- it "should render withour error" do
-   visit new_author_path
- end
+	it "should render withour error" do
+		visit new_author_path
+	end
+
+	it "should have text input fields for 'first name', 'last name, 'homepage'" do
+		visit new_author_path 
+		expect(page).to have_field('First name')
+		expect(page).to have_field('Last name')
+		expect(page).to have_field('Homepage')
+	end
 end
