b68d339234c0a121290b8122c930c9980760e1f3
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 16:43:46 +0100

    Add failing test for #16: 'Author should not validate without last name'

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -13,4 +13,15 @@ describe "Author model", type: :model do
 		author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
 		expect(author.name).to eq('Alan Turing')
 	end
+
+	it "should not allow an author without a last name" do
+		author = Author.new
+		author.last_name = ''
+		author.valid?
+		expect(author.errors[:last_name]).to include("can't be blank")
+
+		author.last_name = 'Turing'
+		author.valid?
+		expect(author.errors[:last_name]).to_not include("can't be blank")
+	end
 end
