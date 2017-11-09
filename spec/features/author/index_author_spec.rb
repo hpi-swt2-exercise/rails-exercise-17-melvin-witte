06b5177634d783e0c69db8a69840ceebe096bc83
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 15:20:59 +0100

    Add failing test for #11: 'Author index page should list name and homepage of all authors'

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -5,4 +5,11 @@ describe "Author index page", type: :feature do
 	it "should render the author index page without error" do
 		visit author_index_path
 	end
+
+	it "should render every author's details, including name and homepage" do
+		author = FactoryGirl.create(:author)
+		visit author_index_path
+		expect(page).to have_content(author.name)
+		expect(page).to have_content(author.homepage)
+	end
 end
