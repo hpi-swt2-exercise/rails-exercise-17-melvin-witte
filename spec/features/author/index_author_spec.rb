838c67a5e1e71161435bd7aa75874aeeb9cf0790
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-09 15:51:48 +0100

    Add failing test for #12: 'Author index page should list first and last name in one column'

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -12,4 +12,13 @@ describe "Author index page", type: :feature do
 		expect(page).to have_content(author.name)
 		expect(page).to have_content(author.homepage)
 	end
+
+	it "should list first and last name in one column and homepage in one column" do
+		author = FactoryGirl.create(:author)
+		visit author_index_path
+		expect(page).to have_selector("th", text: "Name")
+		expect(page).to have_selector("th", text: "Homepage")
+		expect(page).to have_selector("td##{'name'}", text: author.name)
+		expect(page).to have_selector("td##{'homepage'}", text: author.homepage)
+	end
 end
