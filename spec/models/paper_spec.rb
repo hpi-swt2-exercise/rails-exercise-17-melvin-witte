9b3d08d7fd6f35d5c36b6c66bb3839efe415355f
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-10 15:16:34 +0100

    Add failing test for #23

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 describe "Papermodel", type: :model do
+	it "should not validate without a title" do
+		paper = Paper.new(title: nil, venue: "mind49: 433-460", year: 1950)
+		expect(paper.valid?).to eq(false)
+	end
 end
