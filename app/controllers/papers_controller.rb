a436507a43a391456af7ff2a8dc74c870ccd11de
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-10 15:43:44 +0100

    Save new papers when submitting the form. Closes #26

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,4 +1,24 @@
 class PapersController < ApplicationController
 	def new
 	end
+
+	def create
+	  	@paper = Paper.new(paper_params)
+
+	  	if (@paper.save)
+	  	 redirect_to @paper
+	    else
+	      render('new')
+	    end
+	end
+
+	def show
+		@paper = Paper.find(params[:id])
+	end
+
+	private
+
+	def paper_params
+		params.require(:paper).permit(:title, :venue, :year)
+	end
 end
