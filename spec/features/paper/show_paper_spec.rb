9b467273d907dd0fde415dfdd636144a72f74136
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-10 16:49:47 +0100

    Add failing test for #32

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -14,4 +14,13 @@ describe "Show paper page", type: :feature do
 		expect(page).to have_content(paper.venue)
 		expect(page).to have_content(paper.year)
 	end
+
+	it "should display the paper's authors" do
+		author = create(:author)
+		paper = create(:paper)
+		visit paper_path(paper)
+		paper.authors.each do |author|
+			expect(page).to have_content(author.name)
+		end
+	end
 end
