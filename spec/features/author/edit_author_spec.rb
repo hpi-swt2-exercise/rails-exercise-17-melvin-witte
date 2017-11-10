5fbbdf3c2f4a1d840e74f703695b03d689d58a31
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-10 13:45:43 +0100

    Add failing tests for #19: 'Edit author page should save changes'

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,4 +6,26 @@ describe "Edit author page", type: :feature do
 		author = FactoryGirl.create(:author)
 		visit edit_author_path(author)
 	end
+
+	it "should have a form to change author's details" do
+		author = FactoryGirl.create(:author)
+		visit edit_author_path(author)
+		expect(page).to have_field('First name')
+		expect(page).to have_field('Last name')
+		expect(page).to have_field('Homepage')
+	end
+
+	it "should save the changes to the author" do
+		author = FactoryGirl.create(:author)
+		visit edit_author_path(author)
+		fill_in('First name', with: 'Johann')
+		fill_in('Last name', with: 'Goethe')
+		fill_in('Homepage', with: 'https://wikipedia.org/Johann_Wolfgang_von_Goethe')
+		find('input[type="submit"]').click
+		
+		author.reload
+		expect(author.first_name).to eq('Johann')
+		expect(author.last_name).to eq('Goethe')
+		expect(author.homepage).to eq('https://wikipedia.org/Johann_Wolfgang_von_Goethe')
+	end
 end
