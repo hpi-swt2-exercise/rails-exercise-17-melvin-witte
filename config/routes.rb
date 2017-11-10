2aeff4f0a5436b2887a5193b122d14a7568b40e7
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-10 15:09:41 +0100

    Add paper controller with new action. Create form for new papers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'paper_controller/new'
+
   get 'authors/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :authors
+  resources :authors, :papers
 
   # You can have the root of your site routed with "root"
   root 'home#index'
